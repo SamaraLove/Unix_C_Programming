33a7d1eb5df8b0a7fa28c13f83745f55b64420a3
SamaraLove <samaramglove@gmail.com>
2015-08-13 14:04:21 +0800

    Update Lab1Q3.c

--- Lab1/Lab1Q3.c
+++ Lab1/Lab1Q3.c
@@ -5,39 +5,29 @@ int factorial(int);
 int main(void)
 {
 	int number, result;
-	number = 1;
-	while (number >= 0)
-	{	
-		printf("input one integer to calculate its factorial\n");
-		scanf("%d", &number);
-		
-		if (number >= 0)
-		{
-			result = factorial(number);
-			printf("factorial of %d is equal to %d\n", number, result);
-		}
-		else 
-		{
-			printf("Negative integers are not allowed.\n");
-		}
-	}
-
+	
+	printf("input one integer to calculate its factorial\n");
+	scanf("%d", &number);
+	result = factorial(number);
+	printf("factorial of %d is equal to %d\n", number, result);
+	
 return 0;
+	
 }
+
 
 int factorial(int n)
 {
-	int f, temp;
+	int f;
 if (n > 0)
 { 
-//     	printf("n=%d\n",n);
+     	printf("n=%d\n",n);
 	f = n*factorial(n-1);
-	temp = f; // replacing 'return = f' with a variable called temp
+	return f;
 
 }
 else
 {
-	temp = 1;
-}
-return temp;
+	return 1;
+
 }
